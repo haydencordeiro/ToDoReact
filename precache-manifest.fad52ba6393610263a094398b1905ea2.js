d6efb890da12451a16a8cd948a01519712bd796d
hayden cordeiro <cordeirohayden@gmail.com>
2020-05-14 06:10:02 -0700

    Updates

---
+++ precache-manifest.fad52ba6393610263a094398b1905ea2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37f18ca37749e8593036dd8eb40164b9",
+    "url": "/ToDoReact/index.html"
+  },
+  {
+    "revision": "1f4dd79d763db8be7e65",
+    "url": "/ToDoReact/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "1f5a72ebeaa38e9f6129",
+    "url": "/ToDoReact/static/css/main.acdc86df.chunk.css"
+  },
+  {
+    "revision": "1f4dd79d763db8be7e65",
+    "url": "/ToDoReact/static/js/2.b7734b1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoReact/static/js/2.b7734b1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f5a72ebeaa38e9f6129",
+    "url": "/ToDoReact/static/js/main.f61afc9b.chunk.js"
+  },
+  {
+    "revision": "9aa0d7ce341652bea9a8",
+    "url": "/ToDoReact/static/js/runtime-main.f87f9086.js"
+  }
+]);
